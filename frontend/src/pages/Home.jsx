3c24316887ea283da7dedcf62a36443ddfb68349
FrontendFlair <hamaadafzal404@gmail.com>
2024-07-09 02:35:52 -0700

    add functionality on explore menu component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react"
 import ExploreMenu from "../components/ExploreMenu"
 import Header from "../components/Header"
 
 function Home() {
+
+  const [category,setCategory] = useState("All")
+  
   return (
     <div>
       <Header />
-      <ExploreMenu />
+      <ExploreMenu category={category} setCategory={setCategory} />
     </div>
   )
 }
