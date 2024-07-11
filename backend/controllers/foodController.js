41deaca077e698478019c868c5b7e7cb332db8f2
FrontendFlair <hamaadafzal404@gmail.com>
2024-07-11 03:14:51 -0700

    add product functionallity

---
+++ backend/controllers/foodController.js
@@ -0,0 +1,24 @@
+import foodModel from "../models/foodModel.js";
+import fs from "fs";
+
+export const addFood = async (req, res, next) => {
+  const { name, description, price, category } = req.body;
+
+  const image_filename = `${req.file.filename}`;
+
+  const food = new foodModel({
+    name,
+    description,
+    price,
+    category,
+    image: image_filename,
+  });
+
+  try {
+    await food.save();
+    res.json({ success: true, message: "Food Added" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "An Error Occured" });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import { connectDB } from "./config/database.js";
+import foodRouter from "./routes/foodRoute.js";
 
 // config app
 const app = express();
@@ -10,12 +11,16 @@ const PORT = process.env.PORT || 8000;
 
 // middlewares
 app.use(express.json());
+app.use("/images", express.static("uploads"));
 app.use(cors());
 
 // databse connection
 connectDB();
 
 // routes
+
+app.use("/api/food", foodRouter);
+
 app.get("/", (req, res) => {
   res.send("Working!!");
 });
