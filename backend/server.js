130e17152849aabff63dcf08a36ea1e6dae4b157
FrontendFlair <hamaadafzal404@gmail.com>
2024-08-31 04:33:50 -0700

    commit for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRoute from "./routes/orderRoute.js";
+import path from "path";
 
 // config app
 const app = express();
@@ -20,12 +21,20 @@ app.use(cors());
 // databse connection
 connectDB();
 
+const __dirname = path.resolve();
+
 // routes
 
 app.use("/api/food", foodRouter);
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRoute);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.get("/", (req, res) => {
   res.send("Working!!");
